95390716a7b1b2b6fdcfd30b2673f68830af4e06
Murat UYSAL <admin@kopruler.edu.tr>
2025-12-02 14:48:00 +0800

    Fix next.config.js export syntax

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // TypeScript hatalarını build sırasında ignore et
     ignoreBuildErrors: true,
   },
   eslint: {
-    // ESLint hatalarını da ignore et
     ignoreDuringBuilds: true,
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
